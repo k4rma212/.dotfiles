06f24813ebe35a7f88233b625c78f99a2bd0eff8
k4rma212 <karma212@protonmail.com>
2021-07-21 16:26:45 +0300

    Initial commit

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+set fish_greeting ""
+alias gc='git clone'
+alias ls='exa -al --color=always --group-directories-first'
+neofetch
+end
